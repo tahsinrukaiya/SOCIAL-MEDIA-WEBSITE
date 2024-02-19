c5c81921d19aff86c5b992aa944fde8ee8bb7d42
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-02-19 21:46:35 +0100

    working on feed page

--- JS/feed/feed.mjs
+++ JS/feed/feed.mjs
@@ -19,24 +19,26 @@ async function fetchWithToken(url) {
     console.log(posts);
 
     //to loop over the array that is from the response
-    for (let i = 0; i < 10 && i < posts.length; i++) {
-      post_container.innerHTML += `
-   <div class="col pt-3 px-3 pb-3">
+    for (let i = 0; i < 20 && i < posts.length; i++) {
+      post_container.innerHTML += `<div class="col pt-3 px-3 pb-3">
               <div class="p-2 bg-light border border-primary rounded  text-black mx-3">
                   <h6 class="name">ID: ${posts[i].id}</h6>
                   <h6 class="pt-2 mx-3">Title: ${posts[i].title}</h6>
                   <div class="image_container">
-                      <img src="${posts[i].media}" class="post_image">
+                      <img src=${posts[i].media} class="post_image">
                   </div>
                   <p class="mx-3 pt-3">${posts[i].body}</p>
+                  <p class="mx-3 pt-3">Tags: ${posts[i].tags}</p>
                   <p class="mx-3">${posts[i].created}</p>
                   <p class="mx-3">${posts[i].updated}</p>
+
                   <div class="container mx-2">
                       <button type="button" class="btn btn-outline-success btn-sm">Like</button>
                       <button type="button" class="btn btn-outline-success btn-sm">Comment</button>
                   </div>
               </div>
   </div>`;
+
     }
   }
   catch (error) {
