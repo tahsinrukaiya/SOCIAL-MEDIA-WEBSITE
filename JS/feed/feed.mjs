4fbc88a0c48da9304d5d5c69521c7f18f7730eff
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-02-19 13:52:30 +0100

    working on feed page

--- JS/feed/feed.mjs
+++ JS/feed/feed.mjs
@@ -18,30 +18,25 @@ async function fetchWithToken(url) {
     const posts = await response.json();
     console.log(posts);
 
-    //to loop over the array we got from the response
-
-    for (let i = 0; i < posts.length; i++) {
+    //to loop over the array that is from the response
+    for (let i = 0; i < 10 && i < posts.length; i++) {
       post_container.innerHTML += `
-      <div class="container-md mt-2 mx-2 d-flex justify-content-center align-items-center h-100 w-100"
-      id="post_container">
-      <div class="d-grid gap-3 w-75">
-          <div class="p-2 bg-light border text-primary rounded">
-            <h6 class="text-black name">Post id: ${posts[i].id}</h6>
-              <h6 class="text-black name">Title: ${posts[i].title}</h6>
-              <h6 class="pt-2 ms-2">${posts[i].body}</h6>
-              <div class="image">
-                  <img src="${posts[i].media}" class="post_image">
+   <div class="col pt-3 px-3 pb-3">
+              <div class="p-2 bg-light border border-primary rounded  text-black mx-3">
+                  <h6 class="name">ID: ${posts[i].id}</h6>
+                  <h6 class="pt-2 mx-3">Title: ${posts[i].title}</h6>
+                  <div class="image_container">
+                      <img src="${posts[i].media}" class="post_image">
+                  </div>
+                  <p class="mx-3 pt-3">${posts[i].body}</p>
+                  <p class="mx-3">${posts[i].created}</p>
+                  <p class="mx-3">${posts[i].updated}</p>
+                  <div class="container mx-2">
+                      <button type="button" class="btn btn-outline-success btn-sm">Like</button>
+                      <button type="button" class="btn btn-outline-success btn-sm">Comment</button>
+                  </div>
               </div>
-              <p class="mx-3">Tags: ${posts[i].tags}</p>
-              <h6 class="mx-3">Created on: ${posts[i].created}</h6>
-              <h6 class="mx-3">Updated on: ${posts[i].updated}</h6>
-              <div class="container mx-2">
-                  <button type="button" class="btn btn-outline-success btn-sm">Like</button>
-                  <button type="button" class="btn btn-outline-success btn-sm">Comment</button>
-              </div>
-              </div>
-              </div>
-  </div> `;
+  </div>`;
     }
   }
   catch (error) {
@@ -51,72 +46,3 @@ async function fetchWithToken(url) {
 
 fetchWithToken(API_BASE_URL + ALL_POSTS_URL);
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-//fetching data from rainydays API
-
-const jacket_url = "https://api.noroff.dev/api/v1/rainy-days";
-const products = document.querySelector(".products");
-
-
-try {
-  async function getJackets() {
-    //Showing Loading Indication
-    loadingIndicator();
-    //fetch function in use 
-    products.innerHTML = "";
-    const response = await fetch(jacket_url); // fetching the url
-    const results = await response.json(); // making the url readable that is in JSON format
-    const jacket = results; // we got an array of objects, putting that array in a variable
-
-    //to loop over that array
-    //for button
-    // <a href="productDetail.html?id=${jacket[i].id}&title=${jacket[i].title}" class="btn">Buy Now</a>
-
-    for (let i = 0; i < jacket.length; i++) {
-      console.log(jacket[i].title);
-      products.innerHTML += `
-        <div class="product">
-        <a class="main" href="productDetail.html?id=${jacket[i].id}&title=${jacket[i].title}">
-        <img src="${jacket[i].image}" class="product-image"/>
-        <h4 class="product_title">${jacket[i].title}</h4>
-        <p class="product_price">${jacket[i].price}kr</p>
-        <a class="favourite" href="favourite.html"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-        </div>
-        </a>
-      </div> `;
-    }
-  }
-
-  //Function for Loading Indicator
-  function loadingIndicator() {
-    const products = document.querySelector(".products");
-    products.innerHTML = "<li>Loading...</li>";
-  }
-
-  products.onclick = getJackets();
-}
-
-catch {
-  error();
-}
-
-//TO FILTER OUT THE JACKETS WHICH HAS GENDER MALE
-        const filteredJacket = menJacket.filter(filterJacket);
-        function filterJacket(jacket) {
-            if (jacket.gender === "Female") {
-                return true;
-            }
-        }
-
-*/
